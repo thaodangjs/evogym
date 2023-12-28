9e6dbbba4feca0fd7e46d405871e098295a23379
Thao Dang JS <thaodang.js@gmail.com>
2023-12-28 16:02:32 +0700

    finish logo on navbar

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import {Bars3Icon, XmarkIcon} from "@heroicons/react/24/solid"
+import Logo from "@/assets/Logo.png"
+
+
+
+
+
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+
+const flexBetween = "flex items-center justify-between";
+
+  return (
+   <nav >
+    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo}/>
+
+            </div>
+        </div>
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar
