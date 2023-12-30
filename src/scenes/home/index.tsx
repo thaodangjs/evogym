62bfd3380a892077f10b21c2c28097120dae90a1
Thao Dang JS <thaodang.js@gmail.com>
2023-12-30 16:06:36 +0700

    stop at add motion to heading

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -9,6 +9,8 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -20,13 +22,54 @@ const index = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* {IMAGE AND MAIN HEADER} */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* {MAIN HEADER} */}
-        <div></div>
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* {HEADING} */}
+          <motion.div className="md:-mt-20" initial="hidden">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1]">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+            <p className="mt-8 text-sm  ">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+          {/* {ACTION} */}
+          <div className="mt-8 flex items-center gap-8 ">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
 
         {/* {IMAGE} */}
-        <div></div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
       </div>
+      {/* {SPONSOR} */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8 ">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
